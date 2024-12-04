940ee10de8b1f14363d9dd7103ae6a3059763dea
kiruu4 <146743389+kiruu4@users.noreply.github.com>
2024-12-04 21:17:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ function openSurprise(day) {
       alert("Patience ! Ce jour n'est pas encore arrivé.");
     }
   }
+
+function openPage() {
+  const today = new Date();
+  const currentDay = today.getDate();
+  const currentMonth = today.getMonth();
+
+  // Vérifie que le jour est le 5 décembre
+  if (currentMonth === 11 && currentDay >= 5) {
+    window.location.href = "jour5.html"; // Redirige vers une nouvelle page
+  } else {
+    alert("Ce n'est pas encore le bon jour. Patience !");
+  }
+}
